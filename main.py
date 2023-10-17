b71a7209e003b2d264bb9baeaec81deedffa2769
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-17 15:24:23 +0400

    coding other rules of blackjack

--- main.py
+++ main.py
@@ -5,17 +5,32 @@ computer_cards = []
 players_cards = []
 
 
-def deal_card():
-    list = choises(cards, k=2)
+def deal_card(num):
+    return choises(cards, k=num)
 
 
 def calculating_score(arr):
     return sum(arr)
 
 
-players_cards = deal_card()
-computer_cards = deal_card()
-p_score = calculating_score(players_cards)
-c_score = calculating_score(computer_cards)
+players_cards = deal_card(2)
+computer_cards = deal_card(2)
+if 11 in computer_cards and 10 in computer_cards:
+    print("Computer wins.")
+elif 11 in players_cards and 10 in players_cards:
+    print("Player wins. ")
+
 print(f"Your cards: {players_cards}, current score: {p_score}")
 print(f"Computer's first card: {computer_cards[0]}")
+should_pass = input("Type 'y' to get another card, type 'n' to pass. ")
+if should_pass == "n":
+    p_score = calculating_score(players_cards)
+    c_score = calculating_score(computer_cards)
+    if p_score > c_score:
+        print("Computer went over. Computer lose.")
+    elif p_score < c_score:
+        print("You went over. You lose.")
+    else:
+        print("It is draw.")
+elif should_pass == "y":
+    players_cards += deal_card(1)
