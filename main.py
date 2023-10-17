b359c2e9ad31d85fe08583039dfd9b10c589c72e
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-17 15:01:18 +0400

    optimising dealing cards with choise function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import choises
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 computer_cards = []
@@ -6,16 +6,16 @@ players_cards = []
 
 
 def deal_card():
-    i = randint(0, 13)
-    chosen_card = cards[i]
-    return chosen_card
+    list = choises(cards, k=2)
 
 
 def calculating_score(arr):
     return sum(arr)
 
 
-players_cards.append(deal_card())
-players_cards.append(deal_card())
+players_cards = deal_card()
+computer_cards = deal_card()
 p_score = calculating_score(players_cards)
+c_score = calculating_score(computer_cards)
 print(f"Your cards: {players_cards}, current score: {p_score}")
+print(f"Computer's first card: {computer_cards[0]}")
